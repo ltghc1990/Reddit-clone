a78d6fac30cd67016445f12290515c36e95505e3
howell chen <ltghc1990@gmail.com>
2022-08-16 12:28:24 -0700

    added react-query

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Chakra UI
 - Framer motion
 - Tailwind CSS
+  -react-query
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient();
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />;
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
